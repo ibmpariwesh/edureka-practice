982bc66208f3b946b85bc78db81c3a4b5a6d32b9
tcspariwesh <tcspariwesh@gmail.com>
2021-12-08 17:31:34 +0000

    data persistance

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -1,8 +1,25 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	private String name;
 	private int age;
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public int getAge() {
 		return age;
 	}
--- demo/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
 @Service
-public class UserServiceImpl implements IUserService{
+public class UserServiceImpl implements IUserService {
+	@Autowired
+	UserRepository repository;
 
 	@Override
 	public String save(User user) {
 		System.out.println(user.getAge());
+		repository.save(user);
 		return user.getName();
 	}
 }
