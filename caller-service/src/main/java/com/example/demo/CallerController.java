6664893d66f3463aee449d4e89ebae42f49156bf
tcspariwesh <tcspariwesh@gmail.com>
2021-12-27 17:51:11 +0000

    RestTemplate demo

--- caller-service/src/main/java/com/example/demo/CallerController.java
+++ caller-service/src/main/java/com/example/demo/CallerController.java
@@ -5,17 +5,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 @RestController
+//@slf4j
+@RequestMapping("/api")
 public class CallerController {
+	private static final String URL = "http://localhost:8081/user";
 	@Autowired
 	RestTemplate restTemplate;
 	@GetMapping("/callotherservice")
 	void getUserFromOtherservice() {
-		ResponseEntity<List> responseEntity = restTemplate.getForEntity("http://localhost:8081/user", List.class);
-		System.out.println(responseEntity.getStatusCodeValue());
+		ResponseEntity<List> responseEntity = restTemplate.getForEntity(URL, List.class);
+//		(responseEntity.getStatusCodeValue());
 		System.out.println(responseEntity.getBody());
 	}
+	@PostMapping("/createUser")
+	private void sendPostRequestToUserService(@RequestBody User user) {
+//		User user =  new User();
+//		user.setName("user1");
+//		user.setAge(10);
+		restTemplate.postForEntity(URL, user, String.class);
+	}
+	
+//	delete
+//	update
 }
